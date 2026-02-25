dff300e5b13c8d327f26b1d9e598468966e345d5
Neel Desai <neeldesai1085@gmail.com>
2026-02-25 15:30:32 +0530

    Added the @ path and disabled verbatimmodulesyntax

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import Index from "@/pages/Index";
+import NotFound from "@/pages/NotFound";
 
 const App = () => (
       <BrowserRouter>
